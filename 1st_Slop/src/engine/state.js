d498330878772058ef44255e00dc190f15c41247
Jael Pattyn <jael.pattyn@gmail.com>
2026-07-01 17:33:36 +0200

    feat(state): add PAUSE state + transitions

--- 1st_Slop/src/engine/state.js
+++ 1st_Slop/src/engine/state.js
@@ -1,12 +1,14 @@
 export const States = {
   MENU: 'menu', PLAY: 'play', GAMEOVER: 'gameover', LEVEL_COMPLETE: 'levelcomplete',
+  PAUSE: 'pause',
 };
 
 const TRANSITIONS = {
   [States.MENU]: [States.PLAY],
-  [States.PLAY]: [States.GAMEOVER, States.LEVEL_COMPLETE],
+  [States.PLAY]: [States.GAMEOVER, States.LEVEL_COMPLETE, States.PAUSE],
   [States.GAMEOVER]: [States.PLAY, States.MENU],
   [States.LEVEL_COMPLETE]: [States.PLAY],
+  [States.PAUSE]: [States.PLAY, States.MENU],
 };
 
 export function createStateMachine(initial = States.MENU) {
